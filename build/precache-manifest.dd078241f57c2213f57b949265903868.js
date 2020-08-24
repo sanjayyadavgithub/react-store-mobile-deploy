1ac32e8b56b4e80bdcbd317943b856f9b6bff945
sanjayyadavgithub <48355724+sanjayyadavgithub@users.noreply.github.com>
2020-08-24 08:08:39 +0530

    commit

---
+++ build/precache-manifest.dd078241f57c2213f57b949265903868.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecc1ef1d96eb1b98fa683927ccb5cd16",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bcf3af7e76d1959501df",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "caad8da77c100538e433",
+    "url": "/static/css/main.eae103f3.chunk.css"
+  },
+  {
+    "revision": "bcf3af7e76d1959501df",
+    "url": "/static/js/2.75bcc63a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.75bcc63a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "caad8da77c100538e433",
+    "url": "/static/js/main.94a97d6b.chunk.js"
+  },
+  {
+    "revision": "94ca377df234dd41185b",
+    "url": "/static/js/runtime-main.ae2c4f04.js"
+  }
+]);
---
+++ src/components/About.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+/**
+* @author
+* @function About
+**/
+
+const About = (props) => {
+  return(
+      <div className="text-center">
+                 
+                 <h1>Developer Name : Sanjay Yadav</h1>
+                 <h1>From : Basti(UP)</h1>
+                 <h1>Contact Number : 7905851807</h1>
+                 <h1>Email Id : yadavsanjay7905@gmail.com</h1>
+    </div>
+   )
+
+ }
+
+export default About
